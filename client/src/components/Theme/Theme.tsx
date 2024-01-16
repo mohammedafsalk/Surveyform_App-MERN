20ddcd1dfc774acaca794bbc8a5c90a2053c55c8
faris <mrfaristp@gmail.com>
2024-01-16 10:58:20 +0530

    Login added

--- client/src/components/Theme/Theme.tsx
+++ client/src/components/Theme/Theme.tsx
@@ -2,7 +2,7 @@ export default function Theme() {
   return (
     <div className="absolute right-5 sm:right-6 top-5">
       <img
-        className="w-7 h-7 cursor-pointer hidden dark:block"
+        className="w-7 h-7 cursor-pointer hidden dark:block rotate-45"
         src="https://img.icons8.com/ios-glyphs/30/ffffff/sun--v1.png"
         alt="sun--v1"
         onClick={() => document.documentElement.classList.remove("dark")}
